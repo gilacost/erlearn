f3a62d58114e2517f1408eb2a7e4377447b5f0f7
gilacost <josepgiraltdlacoste@gmail.com>
2020-12-25 16:16:33 +0000

    Refactor diamond a little bit

--- Exercism/erlang/diamond/src/diamond.erl
+++ Exercism/erlang/diamond/src/diamond.erl
@@ -1,29 +1,37 @@
 -module(diamond).
 
--export([rows/1, abecedary/0]).
+-export([rows/1, alphabet/0, even_print/1]).
 
 rows(Letter) ->
-    Letters = (abecedary() -- string:find(abecedary(), Letter)) ++ Letter,
+    Letters = (alphabet() -- string:find(alphabet(), Letter)) ++ Letter,
     LettersLength = length(Letters),
     Top = lists:map(
         fun(L) ->
-            pad([L], LettersLength)
+            lists:flatten(pad([L], LettersLength))
         end,
         Letters
     ),
     Top ++ lists:reverse(lists:droplast(Top)).
 
 pad(Str, Length) when Str == "A" ->
-    LeftPad = lists:flatten(string:pad(Str, Length, leading, " ")),
-    RightPad = lists:flatten(string:pad("", Length - 1, trailing, " ")),
+    LeftPad = string:pad(Str, Length, leading, " "),
+    RightPad = string:pad("", Length - 1, trailing, " "),
     string:concat(LeftPad, RightPad);
 pad(Str, Length) ->
-    LetterPos = string:str(abecedary(), Str) - 1,
-    LeftPad = lists:flatten(string:pad(Str, Length - LetterPos, leading, " ")),
-    RightPad = lists:flatten(string:pad("", LetterPos, trailing, " ")),
+    LetterPos = string:str(alphabet(), Str) - 1,
+    LeftPad = string:pad(Str, Length - LetterPos, leading, " "),
+    RightPad = string:pad("", LetterPos, trailing, " "),
 
     First = string:concat(LeftPad, RightPad),
     string:concat(First, string:reverse(lists:droplast(First))).
 
-abecedary() ->
+alphabet() ->
     "ABCDEFGHIJKLMNOPQRSTUVWXYZ".
+
+even_print([]) ->
+    [];
+even_print([H | T]) when H rem 2 /= 0 ->
+    even_print(T);
+even_print([H | T]) ->
+    io:format("printing: ~p~n", [H]),
+    [H | even_print(T)].
