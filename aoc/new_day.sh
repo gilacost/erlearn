12c080c467999ee769188dcf71c016796f7323cf
gilacost <josepgiraltdlacoste@gmail.com>
2020-12-27 03:12:55 +0000

    Day 2

--- aoc/new_day.sh
+++ aoc/new_day.sh
@@ -15,10 +15,14 @@ cat > $1 <<EOF
 
 -export([main/0, readlines/1]).
 
+main() -> "main".
+
 readlines(FileName) ->
     {ok, Data} = file:read_file(FileName),
     BinSplit = binary:split(Data, [<<"\n">>], [global]),
     parse(BinSplit, []).
+
+ocurrences(List, El, string) -> length([X || X <- List, [X] =:= El),
 
 parse([], Buffer) ->
     Buffer;
