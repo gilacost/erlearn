2fe0acce147f5e44677937ad34690a72ea717aa6
gilacost <josepgiraltdlacoste@gmail.com>
2020-12-28 21:44:20 +0000

    Day 5 first cleaner

--- aoc/day_05/first.erl
+++ aoc/day_05/first.erl
@@ -3,37 +3,38 @@
 -export([main/0, readlines/1]).
 
 main() ->
-    Lines = readlines("input_example"),
+    Lines = readlines("input"),
     erlang:display(Lines).
 
 readlines(FileName) ->
     {ok, Data} = file:read_file(FileName),
     BinSplit = binary:split(Data, [<<"\n">>], [global]),
-    parse(BinSplit, []).
+    {_, Max} = parse(BinSplit, [], 0),
+    erlang:display(Max).
 
-row(<<>>, {_Initial, Final}) ->
+binary_partition(<<>>, {_Initial, Final}) ->
     Final;
-row(<<H, T/binary>>, {Initial, Final}) ->
-    row(T, upper_lower(<<H>>, Initial, Final)).
-
-column(<<>>, {_Initial, Final}) ->
-    Final;
-column(<<H, T/binary>>, {Initial, Final}) ->
-    row(T, upper_lower(<<H>>, Initial, Final)).
+binary_partition(<<H, T/binary>>, {Initial, Final}) ->
+    binary_partition(T, upper_lower(<<H>>, Initial, Final)).
 
 upper_lower(<<"B">>, Initial, Final) ->
-    {ceil((Initial + Final) / 2), Final};
+    upper(Initial, Final);
 upper_lower(<<"F">>, Initial, Final) ->
-    {Initial, (Final + Initial) div 2};
+    lower(Initial, Final);
 upper_lower(<<"R">>, Initial, Final) ->
-    {ceil((Initial + Final) / 2), Final};
+    upper(Initial, Final);
 upper_lower(<<"L">>, Initial, Final) ->
-    {Initial, (Final + Initial) div 2}.
+    lower(Initial, Final).
 
-parse([<<>>], Buffer) ->
-    lists:reverse(Buffer);
-parse([<<Row:7/binary, Column:3/binary>> | T], Buffer) ->
-    RowCalc = row(Row, {0, 127}),
-    ColCalc = column(Column, {0, 7}),
+upper(Initial, Final) -> {ceil((Initial + Final) / 2), Final}.
+
+lower(Initial, Final) -> {Initial, (Final + Initial) div 2}.
+
+parse([<<>>], Buffer, MaxId) ->
+    {lists:reverse(Buffer), MaxId};
+parse([<<Row:7/binary, Column:3/binary>> | T], Buffer, MaxId) ->
+    RowCalc = binary_partition(Row, {0, 127}),
+    ColCalc = binary_partition(Column, {0, 7}),
     Id = (RowCalc * 8) + ColCalc,
-    parse(T, [{RowCalc, ColCalc, Id} | Buffer]).
+
+    parse(T, [{RowCalc, ColCalc, Id} | Buffer], max(Id, MaxId)).
