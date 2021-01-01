611a521b80ae39f8b78bc9e097536188f72384c6
gilacost <josepgiraltdlacoste@gmail.com>
2021-01-01 21:32:24 +0000

    Day 8 first

--- aoc/day_08/second.erl
+++ aoc/day_08/second.erl
@@ -1,22 +1,9 @@
-#!/bin/bash
+-module(second).
 
-DAY=$(printf "%02d" $1)
-DIR=day_$DAY
-SESSION_ID=${2:-53616c7465645f5f4b4159acacaebe68fe6d71fad6f2bbd9b56abcd7529c4994a362e9a3dee4c05802e53dee831cc530}
+-export([main/0, readlines/1]).
 
-echo $DAY
-echo $DIR
-echo $SESSION_ID
-
-create ()
-{
-cat > $1 <<EOF
--module($2).
-
--export([main/1, readlines/1]).
-
-main(Input) ->
-    Lines = readlines(Input),
+main() ->
+    Lines = readlines("input"),
     erlang:display(Lines).
 
 readlines(FileName) ->
@@ -39,15 +26,3 @@ parse([<<>>], Buffer) ->
 parse([<<H/binary>> | T], Buffer) ->
     {Number, <<>>} = string:to_integer(H),
     parse(T, [Number | Buffer]).
-EOF
-}
-
-
-mkdir -p $DIR
-create $DIR/first.erl "first"
-create $DIR/second.erl "second"
-cd $DIR
-
-curl "https://adventofcode.com/2020/day/$1/input" \
-  -H "cookie: session=$SESSION_ID" \
-  --compressed -O input
